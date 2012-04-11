bd7fea2fc9d984f8860c1e453967ff550d96be50
Felix C. Stegerman <flx@obfusk.net>
2012-04-12 01:47:11 +0200

    + hello.fnjs; ~ run.clj (looks ok)
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

---
+++ src/fnjs/run.clj
@@ -0,0 +1,35 @@
+; --                                                            ; {{{1
+;
+; File        : fnjs/run.clj
+; Maintainer  : Felix C. Stegerman <flx@obfusk.net>
+; Date        : 2012-04-12
+;
+; Copyright   : Copyright (C) 2012  Felix C. Stegerman
+; Licence     : GPLv2 or EPLv1
+;
+; Depends     : ...
+; Description : ...
+;
+; TODO        : ...
+;
+; --                                                            ; }}}1
+
+(ns fnjs.run
+  (:use [ clojure.java.shell :only [ sh ] ]) )
+
+; --
+
+; MOVE {
+
+(defn safe-read [x] (binding [ *read-eval* false ] (read-string x)))
+(defn read-file [f] (safe-read (str "(" (slurp f) "\n)")))
+
+; } MOVE
+
+; --
+
+(defn uglify! [x] (sh "uglifyjs" "-b" "-i" "2" "-nm" :in x))
+
+; --
+
+; vim: set tw=70 sw=2 sts=2 et fdm=marker :
