305434a9b77eb29f6fb7a3e0aae2f9f1d809a614
Felix C. Stegerman <flx@obfusk.net>
2012-04-12 01:15:15 +0200

    ~ elem.clj (fixed: use; improved: build, do-body; + t/f/null)
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- src/fnjs/elem.clj
+++ src/fnjs/elem.clj
@@ -15,26 +15,30 @@
 ; --                                                            ; }}}1
 
 (ns fnjs.elem
-  (:use clojure.string :only [ join ]) )
+  (:use [ clojure.string :only [ join ] ]) )
 
 ; --
 
+(defn build [xs] (join " " (filter identity (flatten xs))))
 (defn list_ [xs] (interpose "," xs))
+
+; --
+
+(def null   "null")
+(def true_  "true")
+(def false_ "false")
 
 (defn group [& xs] [ "(" xs ")" ])
 (defn block [& xs] [ "{" xs "}" ])
 
-(defn return    [x] [ "return" x  ])
-(defn statement [x] [ x ";"       ])
+(defn return    [x] [ "return" x ])
+(defn statement [x] [ x ";" ])
 
 (defn statements [xs] (map statement xs))
 
 (defn array [& xs]    [ "[" (list_ xs) "]" ])
 (defn var_  [k v]     [ "var" k "=" v ] )
 (defn call  [f args]  [ f (group (list_ args)) ])
-
-(defn function [args body]
-  (group "function" (group (list_ args)) (block body)) )
 
 (defn if-expr [c a b]
   (let [ [c_ a_ b_] (map group c a b) ]
@@ -43,16 +47,15 @@
 (defn if-stmt [c a b] [ "if" (group c) (block a) "else" (block b) ])
 
 (defn do-body [xs]
-  (let [  ys  (statements xs)
+  (let [  ys  (statements (or (seq xs) [ null ]))
           yi  (butlast    ys)
           y   (last       ys) ]
     [ yi (return y) ] ))
 
-(defn do_ [xs] (call (function [] (do-body xs)) []) )
+(defn function [args body]
+  (group "function" (group (list_ args)) (block (do-body body))) )
 
-; --
-
-(defn build [xs] (join " " (flatten xs)))
+(defn do_ [xs] (call (function [] xs) []) )
 
 ; --
 
