72ffda245c4998ff4c0fbfa8c9aacb33c72b491e
Felix C. Stegerman <flx@obfusk.net>
2012-04-12 23:12:49 +0200

    ~ project.clj (main, descr); + main.clj
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject fnjs "0.0.0-dev"
-  :description  "..."
+  :description  "fnjs - functional javascript"
   :source-path  "src"
+  :main         fnjs.main
   :dependencies [ [ org.clojure/clojure "1.3.0" ] ] )
