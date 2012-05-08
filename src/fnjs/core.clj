22cacc0c2bd6eafed1b184e13056414f21698431
Felix C. Stegerman <flx@obfusk.net>
2012-05-09 01:53:57 +0200

    ~ core.clj (do/wrap/return)
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- src/fnjs/core.clj
+++ src/fnjs/core.clj
@@ -2,7 +2,7 @@
 ;
 ; File        : fnjs/core.clj
 ; Maintainer  : Felix C. Stegerman <flx@obfusk.net>
-; Date        : 2012-05-07
+; Date        : 2012-05-08
 ;
 ; Copyright   : Copyright (C) 2012  Felix C. Stegerman
 ; Licence     : GPLv2 or EPLv1
@@ -20,9 +20,14 @@
 
 ; --
 
-(defn fnjs-nowrap [x]       (_e/build (_e/statement (_d/tr x)))) ; ???
-(defn fnjs        [body]    (fnjs-nowrap `(~'do ~@body)))
-(defmacro fnjm    [& body]  (fnjs body))
+(defn fnjs                                                      ; {{{1
+  ([body]       (fnjs body true))
+  ([body wrap?] (_e/build (if wrap?
+                  (_e/statement (_e/do_ (_d/mtr body) false))
+                  (_e/statements (_d/mtr body)) ))))
+                                                                ; }}}1
+
+(defmacro fnjm [& body] (fnjs body))
 
 ; --
 
