421c2684178972b2d8c83a423e94615207c0f214
Felix C. Stegerman <flx@obfusk.net>
2012-05-09 04:02:47 +0200

    ~ webserver.good.js (date + log)
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- output/webserver.good.js
+++ output/webserver.good.js
@@ -4,7 +4,11 @@
     res.writeHead(200, {
       "Content-Type": "text/plain"
     });
-    return res.end("Hello, World!\n\n- fnjs\n");
+    return function() {
+      var d = new Date;
+      console.log("...");
+      return res.end("Hello, World!\n\n- fnjs @ " + d + "\n");
+    }();
   });
   server.listen(3e3, "127.0.0.1");
   console.log("web server @ http://localhost:3000/ ...");
