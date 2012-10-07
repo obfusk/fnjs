af5d70fbb198651f45b4cea2ba7d968b4c72bf85
Felix C. Stegerman <flx@obfusk.net>
2012-10-07 22:28:24 +0200

    project.clj: :url, :licenses
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- project.clj
+++ project.clj
@@ -1,5 +1,10 @@
 (defproject fnjs "0.2.0-dev"
   :description  "fnjs - functional javascript"
+  :url          "https://github.com/obfusk/fnjs"
+  :licenses [ { :name "GPLv2", :distribution :repo
+                :url "http://www.opensource.org/licenses/GPL-2.0" }
+              { :name "EPLv1", :distribution :repo
+                :url "http://www.opensource.org/licenses/EPL-1.0" } ]
   :source-path  "src"
   :main         fnjs.main
   :dependencies [ [ org.clojure/clojure "1.4.0" ] ] )
