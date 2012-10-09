f1c195064367b8c69f3b3e34dbea0e48529bb92d
Felix C. Stegerman <flx@obfusk.net>
2012-10-09 22:19:05 +0200

    core loading more
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- src/fnjs/core.clj
+++ src/fnjs/core.clj
@@ -2,7 +2,7 @@
 ;
 ; File        : fnjs/core.clj
 ; Maintainer  : Felix C. Stegerman <flx@obfusk.net>
-; Date        : 2012-10-05
+; Date        : 2012-10-09
 ;
 ; Copyright   : Copyright (C) 2012  Felix C. Stegerman
 ; Licence     : GPLv2 or EPLv1
@@ -15,8 +15,8 @@
 ; --                                                            ; }}}1
 
 (ns fnjs.core
-  (:require [ fnjs.dsl  :as _d ] [ fnjs.elem :as _e ]
-            [ fnjs.more :as _m ] )) ; load more !!!
+  (:require [ fnjs.dsl :as _d ] [ fnjs.elem :as _e ]
+              fnjs.more )) ; more translations !!!
 
 ; --
 
