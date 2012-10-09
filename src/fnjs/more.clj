3310727c18a1eca0eb676e13c6df8bc1c5007664
Felix C. Stegerman <flx@obfusk.net>
2012-10-09 22:20:49 +0200

    [more] organise; when-let; if-let w/o else
    Signed-off-by: Felix C. Stegerman <flx@obfusk.net>

--- src/fnjs/more.clj
+++ src/fnjs/more.clj
@@ -18,12 +18,18 @@
   (:use     [ fnjs.dsl :only [ defnjm tr ] ])
   (:require [ fnjs.elem :as _e ] [ fnjs.misc :as _m ]) )
 
-; --
+; === Utils ===                                                 ; {{{1
 
-(defn tr_not [x] (tr `(~'juop ~'! ~x)))
+; ...
+                                                                ; }}}1
+
+; === Functions ===                                             ; {{{1
 
 (defn tr_defn  [k & spec] (tr `(~'def  ~k (~'fn ~k ~@spec))))
 (defn tr_defn- [k & spec] (tr `(~'def- ~k (~'fn ~k ~@spec))))
+                                                                ; }}}1
+
+; === Branch/Truth ===                                          ; {{{1
 
 (defn tr_when [x & body] (tr `(~'if ~x (~'do ~@body))))
 
@@ -32,14 +38,22 @@
 
 (defn tr_cond [& clauses] (tr (tr_cond' clauses)))
 
-(defn tr_if-let [[k e] a b]
-  (let [ t (_m/mk-sym '__if_let__) ]
-    (tr `(~'let [~t ~e] (~'if ~t (~'let [~k ~t] ~a) ~b))) ))
+(defn tr_if-let
+  ([bindings a] (tr_if-let bindings a nil))
+  ([[k e] a b]
+    (let [ t (_m/mk-sym '__if_let__) ]
+      (tr `(~'let [~t ~e] (~'if ~t (~'let [~k ~t] ~a) ~b))) )))
+
+(defn tr_when-let [[k e] & body]
+  (let [ t (_m/mk-sym '__when_let__) ]
+    (tr `(~'let [~t ~e] (~'when ~t (~'let [~k ~t] ~@body)))) ))
+
+; --
+
+(defn tr_not [x] (tr `(~'juop ~'! ~x)))
 
 (defn tr_and [& args] (tr (when args `(~'jbop ~'&& ~@args))))
 (defn tr_or  [& args] (tr (when args `(~'jbop ~'|| ~@args))))
-
-; --
 
 (def _? '*root*.*fnjs*.core.?)
 
@@ -56,21 +70,26 @@
 
 (defn tr_?and [& exprs] (tr (tr_?and' exprs)))
 (defn tr_?or  [& exprs] (tr (tr_?or'  exprs)))
+                                                                ; }}}1
 
-; --
+; === ... ===                                                   ; {{{1
+
+; ...
+                                                                ; }}}1
 
 ; defnjm {                                                      ; {{{1
 
-(defnjm ?and    tr_?and   )
-(defnjm ?or     tr_?or    )
-(defnjm and     tr_and    )
-(defnjm cond    tr_cond   )
-(defnjm defn    tr_defn   )
-(defnjm defn-   tr_defn-  )
-(defnjm if-let  tr_if-let )
-(defnjm not     tr_not    )
-(defnjm or      tr_or     )
-(defnjm when    tr_when   )
+(defnjm ?and      tr_?and     )
+(defnjm ?or       tr_?or      )
+(defnjm and       tr_and      )
+(defnjm cond      tr_cond     )
+(defnjm defn      tr_defn     )
+(defnjm defn-     tr_defn-    )
+(defnjm if-let    tr_if-let   )
+(defnjm not       tr_not      )
+(defnjm or        tr_or       )
+(defnjm when      tr_when     )
+(defnjm when-let  tr_when-let )
 
 ; } defnjm                                                      ; }}}1
 
